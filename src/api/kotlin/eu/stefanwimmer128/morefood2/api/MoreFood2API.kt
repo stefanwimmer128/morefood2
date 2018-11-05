f64260d55c3290f86fdde6338f138339662ee4db
Stefan Wimmer <info@stefanwimmer128.eu>
2018-11-05 07:10:32 +0100

    fix kotlin language adapter

--- src/api/kotlin/eu/stefanwimmer128/morefood2/api/MoreFood2API.kt
+++ src/api/kotlin/eu/stefanwimmer128/morefood2/api/MoreFood2API.kt
@@ -2,7 +2,6 @@ package eu.stefanwimmer128.morefood2.api
 
 import eu.stefanwimmer128.morefood2.api.registry.BlockRegistry
 import eu.stefanwimmer128.morefood2.api.registry.ItemRegistry
-import net.minecraftforge.fml.common.ILanguageAdapter
 import org.apache.logging.log4j.Logger
 
 interface MoreFood2API {
@@ -10,13 +9,11 @@ interface MoreFood2API {
         const val MODID = "morefood2"
         const val VERSION = "#{VERSION}"
         const val NAME = "MoreFood 2"
-        
+
         val instance: MoreFood2API by lazy {
             Class.forName("eu.stefanwimmer128.morefood2.MoreFood2").getField("instance").get(null) as MoreFood2API
         }
     }
-    
-    class KotlinLanguageAdapter: ILanguageAdapter.JavaAdapter()
     
     val logger: Logger
     
---
+++ src/api/kotlin/eu/stefanwimmer128/morefood2/api/lang/KotlinLanguageAdapter.kt
@@ -0,0 +1,5 @@
+package eu.stefanwimmer128.morefood2.api.lang
+
+import net.minecraftforge.fml.common.ILanguageAdapter
+
+class KotlinLanguageAdapter: ILanguageAdapter.JavaAdapter()
